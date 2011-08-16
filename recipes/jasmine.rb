4ab384c6518776ff32896baa7e3d52a84d184333
JT Archie <jtarchie@gmail.com>
2011-08-16 18:38:38 -0400

    added jasmine recipe

---
+++ recipes/jasmine.rb
@@ -0,0 +1,12 @@
+gem 'jasmine'
+
+after_bundler do
+  generate "jasmine:install"
+end
+
+__END__
+
+category: testing
+name: Jasmine
+description: Install jasmine for javascript testing
+author: jtarchie
