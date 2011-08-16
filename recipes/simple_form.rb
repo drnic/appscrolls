baf04a082e91d82b1b5bf2a0e5e8f02ac1e999f3
JT Archie <jtarchie@gmail.com>
2011-08-16 18:42:21 -0400

    added simple form recipe

---
+++ recipes/simple_form.rb
@@ -0,0 +1,12 @@
+gem 'simple_form'
+
+after_bundler do
+  generate "simple_form:install"
+end
+
+__END__
+
+category: templating
+name: Simple Form
+description: Install Simple Form to generate nicely formatted forms.
+author: jtarchie
