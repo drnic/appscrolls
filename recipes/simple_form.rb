f1e3d7afa6fb4a9488e3af649a29c92312060485
Jonathan Chang <jonathan.chang@silverpond.com.au>
2011-09-24 19:18:19 +1000

    added simple_form recipe

---
+++ recipes/simple_form.rb
@@ -0,0 +1,17 @@
+gem 'simple_form'
+
+after_bundler do
+  generate "simple_form:install"
+end
+
+__END__
+
+name: Simple Form
+description: "Use Simple Form as a dsl for creating forms."
+author: jonochang
+
+exclusive: forms 
+category: forms
+tags: [forms]
+
+
