cf758882d3d187997f1651f7940c980dd41116d0
Jonathan Chang <jonathan.chang@silverpond.com.au>
2011-10-02 18:39:26 +1100

    added factory girl recipe

---
+++ recipes/factory_girl.rb
@@ -0,0 +1,13 @@
+gem 'factory_girl', :group => [:development, :test]
+gem 'factory_girl_rails', :group => [:development, :test]
+
+__END__
+
+name: Factory Girl
+description: "Use Factory Girl to replace fixtures"
+author: jonochang
+
+exclusive: Factory Girl
+category: Factory Girl
+tags: [Factory Girl]
+
