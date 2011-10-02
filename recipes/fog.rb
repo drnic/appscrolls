ea78a2ca8a7ef9f6f6d6c42fbf21431549ada54a
Jonathan Chang <jonathan.chang@silverpond.com.au>
2011-10-02 16:05:19 +1100

    added fog recipe

---
+++ recipes/fog.rb
@@ -0,0 +1,12 @@
+gem 'fog'
+
+__END__
+
+name: Fog
+description: "Use Fog to connect to cloud services like Amazon and Google"
+author: jonochang
+
+exclusive: cloud 
+category: cloud
+tags: [cloud]
+
