21beea999ddc48d74b40b739cf0879d359fcae80
Jonathan Chang <jonathan.chang@silverpond.com.au>
2011-10-02 18:10:29 +1100

    added fog credentials

--- recipes/carrierwave.rb
+++ recipes/carrierwave.rb
@@ -1,7 +1,24 @@
 gem 'carrierwave'
 
 if config['use_fog']
-  
+  region = config['region']
+  bucket = ask_wizard("Please enter S3 bucket")
+
+  carrierwave_initializer = <<-RB
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => 'CHANGEME',
+    :aws_secret_access_key  => 'CHANGEME',
+    :region                 => '#{region}'
+  }
+  config.fog_directory  = '#{bucket}'
+  config.fog_public     = true
+  # config.fog_host       = 'https://assets.changeme.com.au'
+  # config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
+end
+RB
+
 end
 
 __END__
@@ -20,4 +37,13 @@ config:
       type: boolean
       prompt: "Use Fog with AWS as the storage provider?"
       if_recipe: fog
+  - region:
+      type: multiple_choice
+      prompt: "Which region are you using?"
+      choices:
+        - ["US Standard". us-east-1]
+        - ["Northern California", us-west-1]
+        - ["Ireland", eu-west-1]
+        - ["Singapore", ap-southeast-1]
+        - ["Tokyo", ap-northeast-1]
 
