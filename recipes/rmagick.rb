cc89b68adcda577c48561c99403bf591c4827eec
Jonathan Chang <jonathan.chang@silverpond.com.au>
2011-10-02 16:05:58 +1100

    added rmagick recipe

---
+++ recipes/rmagick.rb
@@ -0,0 +1,12 @@
+gem 'rmagick', :require => "RMagick" 
+
+__END__
+
+name: Delayed Job
+description: "Use Delayed Job to handle background jobs"
+author: jonochang
+
+exclusive: background-tasks 
+category: background-tasks
+tags: [background-tasks]
+
