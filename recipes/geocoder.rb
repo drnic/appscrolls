32191788b01ae34bccaa686cf31bfb8dd62b40f5
Jonathan Chang <jonathan.chang@silverpond.com.au>
2011-10-02 18:39:35 +1100

    added geocoder recipe

---
+++ recipes/geocoder.rb
@@ -0,0 +1,12 @@
+gem "geocoder"
+
+__END__
+
+name: Geocoder
+description: "Use Geocoder for geocoding, reverse geocoding and distance queries"
+author: jonochang
+
+exclusive: geocoding
+category: geocoding
+tags: [geocoding]
+
