390780b1fc8e8f5aca2a5b5f64962e6649a1d1bd
Jonathan Chang <jonathan.chang@silverpond.com.au>
2011-10-02 19:01:28 +1100

    added yaml for rails dev tweaks

--- recipes/rails_dev_tweaks.rb
+++ recipes/rails_dev_tweaks.rb
@@ -1,1 +1,11 @@
 gem 'rails-dev-tweaks', '~> 0.5.0', :group => :development
+
+__END__
+
+name: Rails Dev Tweaks
+description: "Use Rails Dev Tweaks to speed up asset serving in Rails 3.1 dev environment"
+author: jonochang
+
+exclusive: rails-instrumentation
+category: rails-instrumentation
+tags: [rails-instrumentation]
