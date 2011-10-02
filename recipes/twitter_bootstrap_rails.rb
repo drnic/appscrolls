57b95fc998a5483963a8f294330e533a9d9d3086
Jonathan Chang <jonathan.chang@silverpond.com.au>
2011-10-02 16:05:06 +1100

    using jonochang fork for twitter bootstrap rails

--- recipes/twitter_bootstrap_rails.rb
+++ recipes/twitter_bootstrap_rails.rb
@@ -1,4 +1,4 @@
-gem 'twitter-bootstrap-rails', :git => 'http://github.com/seyhunak/twitter-bootstrap-rails.git'
+gem 'twitter-bootstrap-rails', :git => 'https://jonochang@github.com/jonochang/twitter-bootstrap-rails.git'
 
 if config['use_simple_form']
   after_everything do
