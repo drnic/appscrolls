f735f1bc9e025cd3a4f190691b9956d5c8de9de2
Jonathan Chang <jonathan.chang@silverpond.com.au>
2011-10-02 16:05:40 +1100

    added inherited resources recipe

---
+++ recipes/inherited_resources.rb
@@ -0,0 +1,12 @@
+gem 'inherited_resources'
+
+__END__
+
+name: Inherited Resources 
+description: "Use Inherited Resources to DRY your controllers"
+author: jonochang
+
+exclusive: controllers 
+category: controllers
+tags: [controllers]
+
