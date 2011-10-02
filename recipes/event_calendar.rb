caf575246fe2aa09406add9f85851be34c57f560
Jonathan Chang <jonathan.chang@silverpond.com.au>
2011-10-02 18:39:17 +1100

    added event calendar recipe

---
+++ recipes/event_calendar.rb
@@ -0,0 +1,12 @@
+gem 'event-calendar', :require => 'event_calendar'
+
+__END__
+
+name: Event Calendar
+description: "Use Event Calendar to show multiple overlapping events across calendar days and rows"
+author: jonochang
+
+exclusive: calendar
+category: calendar
+tags: [calendar]
+
