9306ffae229790ffc238294845b86e41dce97192
Jonathan Chang <jonathan.chang@silverpond.com.au>
2011-10-02 16:03:03 +1100

    added default layout

--- recipes/twitter_bootstrap_rails.rb
+++ recipes/twitter_bootstrap_rails.rb
@@ -6,6 +6,73 @@ if config['use_simple_form']
     gsub_file "config/initializers/simple_form.rb", "# config.wrapper_class = :input", "# config.wrapper_class = 'clearfix'"
     gsub_file "config/initializers/simple_form.rb", "# config.wrapper_error_class = :field_with_errors", "# config.wrapper_error_class = 'clearfix'"
     gsub_file "config/initializers/simple_form.rb", "# config.error_class = :error", "# config.error_class = 'help-inline'"
+
+    application_layout_body = <<-ERB
+<body>
+
+<%= yield %>
+
+</body>
+ERB
+
+    application_layout_body_new = <<-ERB
+<body>
+  <div class="topbar">
+    <div class="fill">
+      <div class="container">
+
+        <h3><%= link_to "TODO Logo", root_path %></h3>
+
+        <ul class="nav">
+          <li><%= link_to "Users", users_path %></li>
+        </ul>
+
+        <% if not logged_in? %>
+
+        <ul class="nav secondary-nav">
+        </ul>
+          
+        <% else %>
+
+          <ul class="nav secondary-nav">
+            <li class="menu">
+              <a class="menu" href="#">
+                <%= current_user.name %>
+                <i>(signed in with <%= current_user.provider.titleize %>)</i>
+              </a>
+              <ul class="menu-dropdown">
+                <li><%= link_to "My Questions", user_questions_path(current_user) %></li>
+                <li class="divider"></li>
+                <li><%= link_to "Sign out", signout_path %></li>
+              </ul>
+            </li>
+          </ul>
+
+        <% end %>
+
+
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <% if alert %>
+      <div class="alert-message error"><%= alert %></div>
+    <% end %>
+    
+    <% if notice %>
+      <div class="alert-message info"><%= notice %></div>
+    <% end %>
+
+    <%= yield %>
+  </div>
+
+</body>
+
+ERB
+
+    gsub_file 'app/views/layouts/application.html.erb', application_layout_body, application_layout_body_new
+
   end
 
   create_file "app/assets/stylesheets/application.css.new", <<-CSS
