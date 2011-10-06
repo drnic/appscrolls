06da2a068e2b259041810b72569adc63b833d26c
Jonathan Chang <jonathan.chang@silverpond.com.au>
2011-10-06 19:07:55 +1100

    added meta_search and ransack

---
+++ recipes/meta_search.rb
@@ -0,0 +1,11 @@
+gem 'meta_search'
+
+__END__
+
+name: Meta Search
+description: extensible searching for Rails - consider using Ransack
+
+category: search
+exclusive: search
+tags: [search]
+
---
+++ recipes/ransack.rb
@@ -0,0 +1,12 @@
+gem 'ransack'
+
+__END__
+
+name: Ransack
+description: extensible searching for Rails - successor to ransack
+
+category: search
+exclusive: search
+tags: [search]
+
+
