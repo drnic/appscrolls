ac56cdb712a1ca767a6336b9f969da2ebb8886e7
Jonathan Chang <jonathan.chang@silverpond.com.au>
2011-10-10 09:33:26 +1100

    added devise_invitable gem recipe

---
+++ recipes/devise_invitable.rb
@@ -0,0 +1,26 @@
+gem 'devise_invitable'
+
+after_bundler do
+  generate 'devise_invitable:install'
+  generate 'devise_invitable'
+end
+
+after_everything do
+  generate 'devise_invitable:views users'
+end
+
+__END__
+
+name: Devise Invitable
+description: Utilize Devise Invitable to allow users to invite other users to sign up to the system
+author: jonochang
+
+category: authentication
+exclusive: authentication
+run_after: [devise]
+
+config:
+  - generate_user_scoped_view:
+      type: boolean
+      prompt: "Generate Devise Invitable user scoped views?"
+
