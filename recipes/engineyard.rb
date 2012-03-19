777376217b27fd404f6c7ce8bda67dfc46db8be5
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-18 22:30:05 -0700

    add engineyard recipe

---
+++ recipes/engineyard.rb
@@ -0,0 +1,13 @@
+gem 'engineyard'
+gem 'ey_config'
+
+__END__
+
+name: Engine Yard Cloud
+description: The Most Powerful Ruby Cloud
+author: drnic
+
+requires: [git]
+run_after: [git]
+category: deployment
+exclusive: deployment
