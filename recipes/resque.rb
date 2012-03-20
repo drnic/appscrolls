4d5eedf1850f212280894b1fa92056054fe72fe0
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-19 23:59:23 -0700

    add resque recipe to chef run list

--- recipes/resque.rb
+++ recipes/resque.rb
@@ -13,6 +13,8 @@ on_app_servers_and_utilities do
   node[:applications].each do |app_name, data|
     sudo 'echo "sleep 20 && monit -g \#{app_name}_resque restart all" | at now'
   end
+  
+  append_file "deploy/cookbooks/main/recipes/default.rb", "\nrequire_recipe 'resque'\n"
 end
 RUBY
 end
