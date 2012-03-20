0baf046324b0e01d8b8c7f716015ce020e9f5299
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-20 09:43:07 -0700

    add sidekiq recipe

---
+++ recipes/sidekiq.rb
@@ -0,0 +1,15 @@
+gem "sidekiq"
+gem "eycloud-recipe-sidekiq"
+
+__END__
+
+name: Sidekiq
+description: Simple, efficient message processing for your Rails 3 application
+author: drnic
+
+exclusive: worker
+category: worker
+tags: [worker,background-tasks]
+
+requires: [eycloud_recipes_on_deploy]
+run_after: [eycloud_recipes_on_deploy]
