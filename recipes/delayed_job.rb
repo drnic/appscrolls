7636393526994912083bb2f3a05ea1014c5927f3
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-20 08:54:42 -0700

    Only add #delayed_job_admin_authentication if admin requested. Create a readme and show it for delayed_job_admin

--- recipes/delayed_job.rb
+++ recipes/delayed_job.rb
@@ -5,17 +5,52 @@ gem 'delayed_job_admin'
 # app/controllers/application_controller.rb
 # 
 
-inject_into_class "app/controllers/application_controller.rb", "ApplicationController" do
-  <<-RUBY
+if config["admin"]
+  inject_into_class "app/controllers/application_controller.rb", "ApplicationController" do
+    <<-RUBY
   
   def delayed_job_admin_authentication
     # authentication_logic_goes_here
     true
   end
 
-  RUBY
-end
+    RUBY
+  end
+  
+  create_file "readmes/delayed_job_admin.md", <<-MD
+# Delayed Job Admin README
 
+Delayed Job Admin console is available at [/delayed_job_admin](http://localhost:3000/delayed_job_admin).
+
+```
+open http://localhost:3000/delayed_job_admin
+```
+
+## Steps to complete
+
+### Authentication
+
+By default, there is no authentication or authorization protecting access to `/delayed_job_admin`.
+
+Please go to `app/controllers/application_controller.rb` and edit the following method:
+
+```ruby
+def delayed_job_admin_authentication
+  # authentication_logic_goes_here
+  true
+end
+```
+
+For example, if you are using the Devise gem and have an Admin model:
+
+```ruby
+def delayed_job_admin_authentication
+  authenticate_admin!
+end
+```
+
+  MD
+end
 
 after_bundler do
   generate 'delayed_job'
@@ -31,6 +66,11 @@ end
 RUBY
 
   append_file "deploy/cookbooks/main/recipes/default.rb", "\nrequire_recipe 'delayed_job'\n"
+end
+
+after_everything do
+  say_custom "readme", "Delayed Job Admin"
+  run %q{cat "readmes/delayed_job_admin.md"}
 end
 
 __END__
