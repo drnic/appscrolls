c6005a5d07887badca7f193c34800414558c4948
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-19 23:56:06 -0700

    attempt to restart resque on all utilities, even if not installed; just because there is nice deploy hook

--- recipes/resque.rb
+++ recipes/resque.rb
@@ -9,7 +9,7 @@ RAKE
 
   say_wizard 'Installing deploy hooks to restart resque after deploys'
   create_file "deploy/before_restart.rb", <<-RUBY
-on_app_servers_and_utilities(:resque) do
+on_app_servers_and_utilities do
   node[:applications].each do |app_name, data|
     sudo 'echo "sleep 20 && monit -g \#{app_name}_resque restart all" | at now'
   end
