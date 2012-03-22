938f595683c923573bcb994717bca3f981d817c1
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-21 22:46:25 -0700

    add New Relic installer recipe

---
+++ recipes/newrelic.rb
@@ -0,0 +1,11 @@
+gem 'newrelic_rpm'
+
+__END__
+
+name: New Relic RPM
+description: Monitor applications with New Relic
+author: drnic
+website: https://support.cloud.engineyard.com/entries/21016493-monitor-applications-with-new-relic-on-engine-yard-cloud
+
+category: deployment
+exclusive: app-monitoring
