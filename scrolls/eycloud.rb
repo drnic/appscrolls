dbb5d88244db5e1eac586e00c9c68fbbe31d48e8
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-27 11:04:44 -0700

    comment out the ey-v2 work as its got conflicting dependencies with normal rails apps

--- scrolls/eycloud.rb
+++ scrolls/eycloud.rb
@@ -1,7 +1,7 @@
 @name = File.basename(File.expand_path("."))
 say_custom "eycloud", "Deploying #{@name} to Engine Yard Cloud..."
 
-gem 'engineyard-v2', :groups => [:development]
+# gem 'engineyard-v2', :groups => [:development]
 gem 'ey_config'
 
 unless scroll?("mysql") || scroll?("postgresql")
@@ -16,31 +16,31 @@ after_everything do
     say_custom "eycloud", "Using postgresql #{@postgresql_stack}"
   end
   
-  run "ey login"
-
-  require "yaml"
-  require "engineyard"  # for EY.ui
-  require "engineyard/eyrc" # to load api token
-  require "engineyard-cloud-client"
-  ey_api = EY::CloudClient.new(EY::EYRC.load.api_token)
-
-
-  say_custom "eycloud", "Fetching list of accounts..."
-  current_apps = EY::CloudClient::App.all(ey_api)
-  
-  # TODO check if @git_uri is in current_apps, if not then create app
-  
-  accounts = current_apps.map(&:account).uniq
-  account = multiple_choice("Create app to which Engine Yard Cloud account?", accounts.map {|a| [a.name, a]})
-
-  say_custom "eycloud", "Creating application #{@name} on account #{account.name}..."
-  app = EY::CloudClient::App.create(ey_api, {
-    "account"        => account,
-    "name"           => @name,
-    "repository_uri" => @git_uri,
-    "app_type_id"    => "rails3"
-  })
-  
+  # run "ey login"
+  # 
+  # require "yaml"
+  # require "engineyard"  # for EY.ui
+  # require "engineyard/eyrc" # to load api token
+  # require "engineyard-cloud-client"
+  # ey_api = EY::CloudClient.new(EY::EYRC.load.api_token)
+  # 
+  # 
+  # say_custom "eycloud", "Fetching list of accounts..."
+  # current_apps = EY::CloudClient::App.all(ey_api)
+  # 
+  # # TODO check if @git_uri is in current_apps, if not then create app
+  # 
+  # accounts = current_apps.map(&:account).uniq
+  # account = multiple_choice("Create app to which Engine Yard Cloud account?", accounts.map {|a| [a.name, a]})
+  # 
+  # say_custom "eycloud", "Creating application #{@name} on account #{account.name}..."
+  # app = EY::CloudClient::App.create(ey_api, {
+  #   "account"        => account,
+  #   "name"           => @name,
+  #   "repository_uri" => @git_uri,
+  #   "app_type_id"    => "rails3"
+  # })
+  # 
   
   # TODO how to get deploy key?
   # How to upload deploy key to github?
