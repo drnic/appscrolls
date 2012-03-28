f6d4f54a5e39667089f846f03026e05ed61bf1e3
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-27 23:03:32 -0700

    eycloud recipe builds up a command to run to boot& deploy

--- scrolls/eycloud.rb
+++ scrolls/eycloud.rb
@@ -1,53 +1,64 @@
 @name = File.basename(File.expand_path("."))
-say_custom "eycloud", "Deploying #{@name} to Engine Yard Cloud..."
 
-# gem 'engineyard-v2', :groups => [:development]
-gem 'ey_config'
-
-unless scroll?("mysql") || scroll?("postgresql")
-  say_custom "eycloud", "ERROR: To deploy to Engine Yard Cloud, please include 'mysql' or 'postgresql' scroll."
-  exit 1
+gem_group :development do
+  gem 'ey_cli'
+  gem 'engineyard'
 end
 
+gem 'ey_config' # for partner services
+
+required_dbs = %w[mysql postgresql]
+required_app_servers = %w[unicorn trinidad passenger puma thin]
+
+selected_db = required_dbs.find {|db| scroll? db }
+unless selected_db
+  say_custom "eycloud", "Please include a DB choice from: #{required_dbs.join ", "}"
+  exit_now = true
+end
+
+selected_app_server = required_app_servers.find {|app| scroll? app }
+unless selected_app_server
+  say_custom "eycloud", "Please include an App Server choice from: #{required_app_servers.join ", "}"
+  exit_now = true
+end
+
+exit 1 if exit_now
+
 after_everything do
-  if @mysql_stack
-    say_custom "eycloud", "Using mysql #{@mysql_stack}"
-  elsif @postgresql_stack
-    say_custom "eycloud", "Using postgresql #{@postgresql_stack}"
+  @git_url ||= `git config remote.origin.url`.strip
+  if @git_url.size == 0
+    say_custom "eycloud", "Skipping... no git URI discovered"
+  else
+    run "ey login"
+
+    if @mysql_stack
+      say_custom "eycloud", "Using mysql #{@mysql_stack}"
+    elsif @postgresql_stack
+      say_custom "eycloud", "Using postgresql #{@postgresql_stack}"
+    end
+  
+    accounts = `ey_cli accounts | grep "-" | sed -e "s/.* //"`.split(/\n/).map(&:strip)
+    account = multiple_choice "Create app to which Engine Yard Cloud account?", accounts.map {|a| [a, a]}
+    
+    framework_env = multiple_choice "Which framework environment?", [%w[Production production], %w[Staging staging]]
+
+
+    name = File.basename(".")
+    command = "ey_cli create_app --account #{account} --name #{@name} --type rails3 "
+    command += "--git #{@git_url} "
+    command += "--framework_env #{framework_env} "
+    command += "--env_name #{@name}_#{framework_env} "
+    command += "--stack #{selected_app_server} "
+    command += "--solo"
+    run command
+  
+    # TODO how to get deploy key?
+    # How to upload deploy key to github?
+  
+    # multiple_choice("Let's boot some instances!", [
+    #   [""]
+    # ])
   end
-  
-  # run "ey login"
-  # 
-  # require "yaml"
-  # require "engineyard"  # for EY.ui
-  # require "engineyard/eyrc" # to load api token
-  # require "engineyard-cloud-client"
-  # ey_api = EY::CloudClient.new(EY::EYRC.load.api_token)
-  # 
-  # 
-  # say_custom "eycloud", "Fetching list of accounts..."
-  # current_apps = EY::CloudClient::App.all(ey_api)
-  # 
-  # # TODO check if @git_uri is in current_apps, if not then create app
-  # 
-  # accounts = current_apps.map(&:account).uniq
-  # account = multiple_choice("Create app to which Engine Yard Cloud account?", accounts.map {|a| [a.name, a]})
-  # 
-  # say_custom "eycloud", "Creating application #{@name} on account #{account.name}..."
-  # app = EY::CloudClient::App.create(ey_api, {
-  #   "account"        => account,
-  #   "name"           => @name,
-  #   "repository_uri" => @git_uri,
-  #   "app_type_id"    => "rails3"
-  # })
-  # 
-  
-  # TODO how to get deploy key?
-  # How to upload deploy key to github?
-  
-  # multiple_choice("Let's boot some instances!", [
-  #   [""]
-  # ])
 end
 
 __END__
@@ -56,7 +67,7 @@ name: Engine Yard Cloud
 description: The Most Powerful Ruby Cloud
 author: drnic
 
-requires: [github]
+# requires: [github]
 run_after: [github]
 category: deployment
 exclusive: deployment
