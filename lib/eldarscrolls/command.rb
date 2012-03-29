1ebe9328652b65e7a363f0b5aa09d6467099bafc
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-28 18:07:33 -0700

    initial spike of applying scrolls to existing app

--- lib/eldarscrolls/command.rb
+++ lib/eldarscrolls/command.rb
@@ -29,6 +29,31 @@ module EldarScrolls
       end
     end
 
+    desc "apply", "Apply scrolls to this rails app"
+    method_option :scrolls, :type => :array, :aliases => "-s", :desc => "List scrolls, e.g. -s resque rails_basics jquery"
+    method_option :template, :type => :boolean, :aliases => "-t", :desc => "Only display template that would be used"
+    def apply()
+      if options[:scrolls]
+        apply_template(options[:scrolls], options[:template])
+      else
+        @scrolls = []
+
+        while scroll = ask("#{print_scrolls}#{bold}Which scroll would you like to add? #{clear}#{yellow}(blank to finish)#{clear}")
+          if scroll == ''
+            apply_template(@scrolls)
+            break
+          elsif RailsWizard::Scrolls.list.include?(scroll)
+            @scrolls << scroll
+            puts
+            puts "> #{green}Added '#{scroll}' to template.#{clear}"
+          else
+            puts
+            puts "> #{red}Invalid scroll, please try again.#{clear}"
+          end
+        end
+      end
+    end
+
     desc "list [CATEGORY]", "list available scrolls (optionally by category)"
     def list(category = nil)
       if category
@@ -76,6 +101,25 @@ module EldarScrolls
           puts File.read(file.path)
         else
           system "rails new #{name} -m #{file.path} #{template.args.join(' ')}"
+        end
+      ensure
+        file.unlink
+      end
+
+      def apply_template(scrolls, display_only = false)
+        puts
+        puts
+        puts "#{bold}Generating and Running Template..."
+        puts
+        file = Tempfile.new('template')
+        template = RailsWizard::Template.new(scrolls)
+        file.write template.compile
+        file.close
+        if display_only
+          puts "Template stored to #{file.path}"
+          puts File.read(file.path)
+        else
+          system "rails g run_template #{file.path}"
         end
       ensure
         file.unlink
