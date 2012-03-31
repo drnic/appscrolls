7e2ee3158af8741c042ddaceca18f3e6b49fda15
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-31 16:06:33 -0700

    initial support for omniauth 1.0+

--- scrolls/zzz/omniauth.rb
+++ scrolls/zzz/omniauth.rb
@@ -1,17 +1,55 @@
-gem 'omniauth', '~> 0.2.0'
+strategy_gem = ""
+while strategy_gem.size == 0
+  strategy_gem = ask_wizard "Which rubygem contains the strategy you choose? (e.g. 'omniauth-github'; press ENTER to see all)"
+
+  if strategy_gem.size == 0
+    say_custom "omniauth", "Displaying all strategies"
+    run "gem list --remote omniauth-"
+  end
+end
+
+gem 'omniauth', '~> 1.0'
+gem strategy_gem
+
+if strategy_gem =~ /omniauth-(.*)(?:-provider)?/
+  strategy = $1
+else
+  strategy = "unknown"
+end
 
 after_bundler do
-  file 'app/controllers/sessions_controller.rb', "class SessionsController < ApplicationController\n  def callback\n    auth # Do what you want with the auth hash!\n  end\n\n  def auth; request.env['omniauth.auth'] end\nend"
   route "match '/auth/:provider/callback', :to => 'sessions#callback'"
+  file 'app/controllers/sessions_controller.rb', <<-RUBY
+class SessionsController < ApplicationController
+  def callback
+    auth # Do what you want with the auth hash!
+  end
+  
+  def auth
+    request.env['omniauth.auth']
+  end
+end
+RUBY
+
+  initializer "omniauth.rb", <<-RUBY
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :#{strategy}, ENV['#{strategy.upcase}_KEY']
+end
+RUBY
+
+end
+
+# TODO consult https://raw.github.com/RailsApps/rails3-application-templates/master/rails3-mongoid-omniauth-template.rb
+
+after_everything do
+  say_custom "omniauth", "NOTICE: Please consult the strategy for final instructions https://github.com/search?q=#{strategy}"
 end
 
 __END__
 
 name: OmniAuth
-description: "A basic setup of OmniAuth with a SessionsController to handle the request and callback phases."
-author: mbleigh
+description: "Install OmniAuth and a selected Strategy/Provider"
+author: drnic
 
 exclusive: authentication
 category: authentication
-
-# TODO watch railscasts
