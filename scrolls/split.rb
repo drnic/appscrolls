4b197a7e15ee74233b0ba5960a56ee39488a13b3
Dr Nic Williams <drnicwilliams@gmail.com>
2012-04-02 22:00:09 -0700

    Generate a split_config.rb initializer with defaults suggested in RailsCasts

--- scrolls/split.rb
+++ scrolls/split.rb
@@ -3,6 +3,16 @@ gem 'split', :require => 'split/dashboard'
 
 after_bundler do
   route "mount Split::Dashboard, :at => '/split/#{config['split_admin_secret']}'"
+  
+  initializer "split_config.rb", <<-RUBY
+Split.configure do |config|
+  config.db_failover = true # handle redis errors gracefully
+  config.db_failover_on_db_error = proc{|error| Rails.logger.error(error.message) }
+  config.allow_multiple_experiments = true
+  # config.robot_regex = /my_custom_robot_regex/
+  # config.ignore_ip_addresses << '81.19.48.130'
+end
+RUBY
 end
 
 __END__
