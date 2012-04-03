3c37861b9457d5e68c0baecf6442281dbd566e47
Dr Nic Williams <drnicwilliams@gmail.com>
2012-04-02 21:07:20 -0700

    initial mongodb scroll

---
+++ scrolls/mongodb.rb
@@ -0,0 +1,15 @@
+gem "mongo"
+gem "bson_ext"
+
+__END__
+
+name: MongoDB
+description: Ruby driver for MongoDB
+website: https://github.com/mongodb/mongo-ruby-driver
+author: drnic
+
+requires: []
+run_after: []
+run_before: []
+
+category: persistence
