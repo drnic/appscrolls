324b089b397378548d31160d15209e14a6cf3b2b
Dr Nic Williams <drnicwilliams@gmail.com>
2012-04-10 09:27:50 -0700

    fix spelling error in url

--- scrolls/rails_basics.rb
+++ scrolls/rails_basics.rb
@@ -26,7 +26,7 @@ ey deploy
 
 ## Thanks
 
-The original scaffold for this application was created by [App Scrolls](http://railsappscrolls.org).
+The original scaffold for this application was created by [App Scrolls](http://appscrolls.org).
 
 The project was created with the following scrolls:
 
