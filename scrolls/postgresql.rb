36cd79550a62dc6fa1f94762939972bc867dc41b
Craig S. Cottingham <craig.cottingham@hrworx.com>
2012-04-12 00:20:53 -0500

    Add check that username already exists
    Signed-off-by: Craig S. Cottingham <craig.cottingham@hrworx.com>

--- scrolls/postgresql.rb
+++ scrolls/postgresql.rb
@@ -1,15 +1,25 @@
 gem "pg"
 
 after_bundler do
+  require 'pg'
+
   pg_username = ask_wizard "Local development PostgreSQL username:"
   pg_password = ask_wizard "Local development PostgreSQL password:"
 
-  gsub_file "config/database.yml", /username:\s?.*/, "username: #{pg_username}"
-  gsub_file "config/database.yml", /password:\s?.*/, "password: #{pg_password}"
+  # attempt to connect to PostgreSQL using username and password
+  params = { :dbname => 'template1', :user => pg_username }
+  params[:password] = pg_password unless pg_password.blank?
 
-  rake "db:create:all"
+  begin
+    dbh = PG::Connection.new params
+    dbh.finish
 
-  rakefile("sample.rake") do
+    gsub_file "config/database.yml", /username:\s?.*/, "username: #{pg_username}"
+    gsub_file "config/database.yml", /password:\s?.*/, "password: #{pg_password}"
+
+    rake "db:create:all"
+
+    rakefile("sample.rake") do
 <<-RUBY
 namespace :db do
   desc "Populate the database with sample data"
@@ -19,6 +29,11 @@ namespace :db do
   task :populate => :sample
 end
 RUBY
+    end
+  rescue PG::Error => err
+    # surely there's a better way to report errors than this....
+    puts err.to_s
+    exit 1
   end
 end
 
