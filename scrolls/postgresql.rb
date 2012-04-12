c8bee991fecfee4082d5e59b70eaeae82d5742bb
Craig S. Cottingham <craig.cottingham@hrworx.com>
2012-04-12 09:18:11 -0500

    Move prompt for username and password to inside loop
    Signed-off-by: Craig S. Cottingham <craig.cottingham@hrworx.com>

--- scrolls/postgresql.rb
+++ scrolls/postgresql.rb
@@ -3,23 +3,33 @@ gem "pg"
 after_bundler do
   require 'pg'
 
-  pg_username = ask_wizard "Local development PostgreSQL username:"
-  pg_password = ask_wizard "Local development PostgreSQL password:"
+  pg_username = nil
+  pg_password = nil
 
-  # attempt to connect to PostgreSQL using username and password
-  params = { :dbname => 'template1', :user => pg_username }
-  params[:password] = pg_password unless pg_password.blank?
+  while pg_username.nil?
+    pg_username = ask_wizard "Local development PostgreSQL username:"
+    pg_password = ask_wizard "Local development PostgreSQL password:"
 
-  begin
-    dbh = PG::Connection.new params
-    dbh.finish
+    # attempt to connect to PostgreSQL using username and password
+    params = { :dbname => 'template1', :user => pg_username }
+    params[:password] = pg_password unless pg_password.blank?
 
-    gsub_file "config/database.yml", /username:\s?.*/, "username: #{pg_username}"
-    gsub_file "config/database.yml", /password:\s?.*/, "password: #{pg_password}"
+    begin
+      dbh = PG::Connection.new params
+      dbh.finish
+    rescue PG::Error => err
+      say_wizard err.to_s
+      pg_username = nil
+      pg_password = nil
+    end
+  end
 
-    rake "db:create:all"
+  gsub_file "config/database.yml", /username:\s?.*/, "username: #{pg_username}"
+  gsub_file "config/database.yml", /password:\s?.*/, "password: #{pg_password}"
 
-    rakefile("sample.rake") do
+  rake "db:create:all"
+
+  rakefile("sample.rake") do
 <<-RUBY
 namespace :db do
   desc "Populate the database with sample data"
@@ -29,11 +39,6 @@ namespace :db do
   task :populate => :sample
 end
 RUBY
-    end
-  rescue PG::Error => err
-    # surely there's a better way to report errors than this....
-    puts err.to_s
-    exit 1
   end
 end
 
