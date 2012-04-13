3c052f994f9417e37a3f21805ddc48d9cd2fc32e
Grzesiek Kolodziejczyk <gkolodziejczyk@gmail.com>
2012-04-13 09:48:06 +0200

    Load scrolls from all gems and from ~/.scrolls/

--- lib/appscrolls.rb
+++ lib/appscrolls.rb
@@ -3,7 +3,15 @@ require 'appscrolls/scroll'
 require 'appscrolls/config'
 require 'appscrolls/template'
 
-Dir[File.dirname(__FILE__) + '/../scrolls/*.rb'].each do |path|
+Gem.path.each do |gemdir|
+  Dir[gemdir + '/gems/*/scrolls/*.rb'].each do |path|
+    key = File.basename(path, '.rb')
+    scroll = AppScrollsScrolls::Scroll.generate(key, File.open(path))
+    AppScrollsScrolls::Scrolls.add(scroll)
+  end
+end
+
+Dir[File.expand_path("~/.scrolls") + '/**/*.rb'].each do |path|
   key = File.basename(path, '.rb')
   scroll = AppScrollsScrolls::Scroll.generate(key, File.open(path))
   AppScrollsScrolls::Scrolls.add(scroll)
