d70ffa2a5ca61120bfe4da5b1b925b421c70fb76
Erik Michaels-Ober <sferik@gmail.com>
2012-04-14 18:13:08 -0700

    Create official scroll for RailsAdmin

---
+++ scrolls/rails_admin.rb
@@ -0,0 +1,15 @@
+gem 'rails_admin', :git => 'git://github.com/sferik/rails_admin.git'
+
+after_bundler do
+  generate "rails_admin:install"
+end
+
+__END__
+
+name: RailsAdmin
+description: "An engine that provides an easy-to-use interface for managing your data"
+website: https://github.com/sferik/rails_admin
+author: sferik
+exclusive: administration
+category: administration
+tags: [administration]
