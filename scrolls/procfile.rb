791b63dcb155043e248ee62ec5d3db7812385890
Dr Nic Williams <drnicwilliams@gmail.com>
2012-05-10 15:39:30 -0700

    add procfile scroll

---
+++ scrolls/procfile.rb
@@ -0,0 +1,12 @@
+gem 'foreman'
+
+create_file "Procfile", ""
+
+__END__
+
+name: Procfile
+description: Offer a Procfile for describing the processes for this application
+website: https://github.com/ddollar/foreman
+author: drnic
+
+category: deployment
