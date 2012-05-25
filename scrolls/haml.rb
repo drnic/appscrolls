5734e0960e074c5273ba8f157ec6df2492b02ea2
Aleksei Kvitinskii <aleksei.wm@gmail.com>
2012-05-26 01:18:43 +0300

    improved haml scroll

---
+++ scrolls/haml.rb
@@ -0,0 +1,30 @@
+gem 'haml', '>= 3.0.0'
+gem 'haml-rails'
+
+inject_into_file "config/initializers/generators.rb", :after => "Rails.application.config.generators do |g|\n" do
+  "    g.template_engine :haml\n"
+end
+
+after_bundler do
+	remove_file "app/views/layouts/application.html.erb" 
+  create_file "app/views/layouts/application.html.haml", <<-RUBY
+!!!
+%html
+  %head
+    %title Application
+    = stylesheet_link_tag :application
+    = javascript_include_tag :application
+    = csrf_meta_tag
+  %body
+    = yield
+RUBY
+end
+
+__END__
+
+name: HAML
+description: "Utilize HAML for templating."
+author: axy
+
+category: templating
+exclusive: templating
