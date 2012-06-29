ed85e4c680fa2748b7f908637cff7b1d9bde211e
Allan Grant <allan@allangrant.net>
2012-06-28 19:35:32 -0700

    added devise

--- scrolls/devise.rb
+++ scrolls/devise.rb
@@ -1,10 +1,8 @@
 gem 'devise'
 
 inject_into_file 'config/environments/development.rb', "\nconfig.action_mailer.default_url_options = { :host => 'localhost:3000' }\n", :after => "Application.configure do"
-inject_into_file 'config/environments/test.rb',        "\nconfig.action_mailer.default_url_options = { :host => 'localhost:7000' }\n", :after => "Application.configure do"
+inject_into_file 'config/environments/test.rb',        "\nconfig.action_mailer.default_url_options = { :host => 'localhost:3000' }\n", :after => "Application.configure do"
 inject_into_file 'config/environments/production.rb',  "\nconfig.action_mailer.default_url_options = { :host => '#{app_name}.com' }\n", :after => "Application.configure do"
-
-inject_into_file 'config/routes.rb', "\nroot :to => 'home#index'\n", :after => "Testapp::Application.routes.draw do"
 
 after_bundler do
   generate 'devise:install'
@@ -20,20 +18,6 @@ after_bundler do
   generate 'devise user'
   generate "devise:views"
 
-  if config['add_app_helpers']
-    new_helpers = <<-RB
-module ApplicationHelper
-
-  def current_user
-    @current_user
-  end
-
-  def logged_in?
-    @current_user != nil
-  end
-RB
-    gsub_file 'app/helpers/application_helper.rb', 'module ApplicationHelper', new_helpers
-  end
 end
 
 __END__
@@ -44,9 +28,3 @@ author: mbleigh
 
 category: authentication
 exclusive: authentication
-
-config:
-  - add_app_helpers:
-      type: boolean
-      prompt: "Add logged_in and current_user helpers?"
-
