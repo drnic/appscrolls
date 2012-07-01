9d7748fb077b4e007bb59cd9acf60c7d5e24042d
Allan Grant <allan@allangrant.net>
2012-07-01 01:52:01 -0700

    diff patch: active admin views for users and admin users

--- scrolls/active_admin.rb
+++ scrolls/active_admin.rb
@@ -3,6 +3,12 @@ gem 'activeadmin'
 after_bundler do
   generate "active_admin:install"
   inject_into_file "config/application.rb", "\n    config.assets.precompile += ['active_admin.js', 'active_admin.css']", :before => "\n  end"
+  if scrolls.include? 'devise'
+    apply_patch :devise
+    if scrolls.include? 'omniauth'
+      apply_patch :omniauth
+    end
+  end
 end
 
 __END__
