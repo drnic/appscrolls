5d508968af5762ffaf63a7ce42befb6e1ff419d1
Matt Olson <matt@mattolson.com>
2012-09-28 15:57:49 -0700

    Add rbenv support
    -- Prompt for ruby verison
    -- Add .rbenv-version file
    -- Add ruby version to Gemfile
    -- Fix bad path in rails_basics

--- scrolls/rails_basics.rb
+++ scrolls/rails_basics.rb
@@ -9,11 +9,12 @@ after_bundler do
   # Remove default static home page and move rails readme out of the way
   if scroll? 'git'
     git :rm => 'public/index.html'
-    git :rm => 'public/images/rails.png'
+    git :rm => 'app/assets/images/rails.png'
+    create_file 'app/assets/images/.gitkeep'
     git :mv => 'README.rdoc doc/RAILS_README.rdoc'
   else
     remove_file "public/index.html"
-    remove_file "public/images/rails.png"
+    remove_file "app/assets/images/rails.png"
     run 'mv README.rdoc doc/RAILS_README.rdoc'
   end
 
---
+++ scrolls/rbenv.rb
@@ -0,0 +1,18 @@
+after_bundler do
+  run "rbenv local #{config['ruby_version']}"
+  insert_into_file "Gemfile", "ruby '#{config['ruby_version'].gsub /-.*/, ''}'\n", :after => "source 'https://rubygems.org'\n\n"
+end
+
+__END__
+
+name: rbenv
+description: set project-specific ruby version using rbenv
+website: https://github.com/sstephenson/rbenv
+author: mattolson
+
+category: deployment
+
+config:
+  - ruby_version:
+      type: string
+      prompt: "Which version of ruby do you want? (Must be already installed and registered in rbenv. i.e '1.9.3-p194')"
