fd160d810ed88691eb987f37eef30b16eaa9756a
Dr Nic Williams <drnicwilliams@gmail.com>
2012-12-05 11:20:13 -0800

    github scroll uses hub now

--- scrolls/github.rb
+++ scrolls/github.rb
@@ -1,22 +1,28 @@
-gem 'github', '>= 0.7.0', :require => nil, :group => [:development]
+if `which hub`.strip == ""
+  say_custom "hub", "Please install hub. https://github.com/defunkt/hub"
+  exit 1
+end
 
 after_everything do
   github_private = multiple_choice "Create a GitHub repository?", [["Public", false], ["Private", true]]
+  
+  # Usage: hub create [NAME] [-p] [-d DESCRIPTION] [-h HOMEPAGE]
   
   tried_create_already = false
   while (@git_uri = `git config remote.origin.url`.strip) && @git_uri.size == 0
     if tried_create_already
       @repo_name = ask_wizard "Repository already exists. What project name?"
     else
-      @repo_name = ""
+      @repo_name = File.basename(File.expand_path("."))
     end
     if github_private
-      run "bundle exec gh create-from-local #{@repo_name} --private"
+      run "hub create #{@repo_name} -p"
     else
-      run "bundle exec gh create-from-local #{@repo_name}"
+      run "hub create #{@repo_name}w"
     end
     tried_create_already = true
   end
+  run "git push origin master"
   
   say_custom "github", "Created repo #{@git_uri}"
 end
